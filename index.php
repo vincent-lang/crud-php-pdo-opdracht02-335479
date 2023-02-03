ab2dbe1b2337f4c751b780ef57847bd411a269f7
vincent van de merwe <335479@student.mboutrecht.nl>
2023-02-03 12:04:30 +0100

    fourth commit

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,50 +9,55 @@
     <link rel="shortcut icon" href="img/anon.png" type="image/x-icon">
     <title>pizza bakkery</title>
 </head>
+
 <body>
     <h1>Maak je eigen pizza</h1>
     <form action="">
-    <label for="Bodemformaat">Bodemformaat</label>
-    <br>
-    <select name="Bodemformaat" id="Bodemformaat">
-        <option value="Maak je keuze">Maak je keuze</option>
-        <option value="20 centimeter">20 centimeter</option>
-        <option value="25 centimeter">25 centimeter</option>
-        <option value="30 centimeter">30 centimeter</option>
-        <option value="35 centimeter">35 centimeter</option>
-        <option value="40 centimeter">40 centimeter</option>
-    </select>
-    <br><br>
-    <label for="Saus">Saus</label>
-    <br>
-    <select name="Saus" id="Saus">
-        <option value="Maak je keuze">Maak je keuze</option>
-        <option value="Tomatensaus">Tomatensaus</option>
-        <option value="Extra tomatensaus">Extra tomatensaus</option>
-        <option value="Spicy tomatensaus">Spicy tomatensaus</option>
-        <option value="BBQ saus">BBQ saus</option>
-        <option value="Créme fraiche">Créme fraiche</option>
-    </select>
-    <br><br>
-    <label for="Pizzatoppings">Pizzatoppings</label>
-    <br>
-        <input type="radio" id="Pizzatoppings" name="Pizzatoppings" value="Pizzatoppings">vegan
-        <br>
-        <input type="radio" id="Pizzatoppings" name="Pizzatoppings" value="Pizzatoppings">vegetarisch
-        <br>
-        <input type="radio" id="Pizzatoppings" name="Pizzatoppings" value="Pizzatoppings">vlees
-        <br><br>
-        <label for="Kruiden">Kruiden</label>
-        <br>
-        <input type="checkbox" id="Pizzatoppings" name="choice1">Peterselie
-        <br>
-        <input type="checkbox" id="Pizzatoppings" name="choice2">Oregano
-        <br>
-        <input type="checkbox" id="Pizzatoppings" name="choice3">Chili flakes
-        <br>
-        <input type="checkbox" id="Pizzatoppings" name="choice4">Zwarte peper
-        <br><br>
-        <input type="submit" value="Bestel">
-        </form>
+    <fieldset>
+            <label for="Bodemformaat">Bodemformaat</label>
+            <br>
+            <select name="Bodemformaat" id="Bodemformaat">
+                <option value="Maak je keuze">Maak je keuze</option>
+                <option value="20 centimeter">20 centimeter</option>
+                <option value="25 centimeter">25 centimeter</option>
+                <option value="30 centimeter">30 centimeter</option>
+                <option value="35 centimeter">35 centimeter</option>
+                <option value="40 centimeter">40 centimeter</option>
+            </select>
+            <br>
+            <label for="Saus">Saus</label>
+            <br>
+            <select name="Saus" id="Saus">
+                <option value="Maak je keuze">Maak je keuze</option>
+                <option value="Tomatensaus">Tomatensaus</option>
+                <option value="Extra tomatensaus">Extra tomatensaus</option>
+                <option value="Spicy tomatensaus">Spicy tomatensaus</option>
+                <option value="BBQ saus">BBQ saus</option>
+                <option value="Créme fraiche">Créme fraiche</option>
+            </select>
+            <br>
+            <label for="Pizzatoppings">Pizzatoppings</label>
+            <br>
+            vegan
+            <input type="radio" id="Pizzatoppings" name="Pizzatoppings" value="Pizzatoppings">
+            vegetarisch
+            <input type="radio" id="Pizzatoppings" name="Pizzatoppings" value="Pizzatoppings">
+            vlees
+            <input type="radio" id="Pizzatoppings" name="Pizzatoppings" value="Pizzatoppings">
+            <br>
+            <label for="Kruiden">Kruiden</label>
+            <br>
+            Peterselie
+            <input type="checkbox" id="Pizzatoppings" name="choice1">
+            Oregano
+            <input type="checkbox" id="Pizzatoppings" name="choice2">
+            Chili flakes
+            <input type="checkbox" id="Pizzatoppings" name="choice3">
+            Zwarte peper
+            <input type="checkbox" id="Pizzatoppings" name="choice4">
+            <input id="submit" type="submit" value="Bestel">
+            </fieldset>
+    </form>
 </body>
+
 </html>
